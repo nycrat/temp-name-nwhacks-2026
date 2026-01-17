05c49080928fc77f8edbd81ae27e949268494e85
Eric Xiao <ericxiao0000@gmail.com>
2026-01-17 15:38:53 -0800

    Chore formatting

--- app/api/course/list/route.ts
+++ app/api/course/list/route.ts
@@ -3,6 +3,6 @@ import { neon } from "@neondatabase/serverless";
 
 export async function GET() {
 	const sql = neon(`${process.env.DATABASE_URL}`);
-	const rows = await sql`SELECT * FROM courses` as Course[];
+	const rows = (await sql`SELECT * FROM courses`) as Course[];
 	return Response.json(rows);
 }
