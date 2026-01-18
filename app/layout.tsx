ce58a9c776a5efd72432dbc2041c8abd0c96462b
Eric Xiao <ericxiao0000@gmail.com>
2026-01-18 07:07:42 -0800

    Change title to ATTD 101

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 export const metadata: Metadata = {
-  title: "ATTD - Lecture Discovery",
+  title: "ATTD 101 - Lecture Discovery",
   description: "An academic dashboard for discovering live lectures.",
 };
 
